d520f070eb172c7d9e50fcfd99d667d6e33ecdda
Quinton Petty <qpetty@Quintons-MacBook-Pro.local>
2014-02-04 18:35:16 -0800

    Figured out Audio Queue Services and playing unstoppable song

--- SongTrain/ServerPlaylistViewController.h
+++ SongTrain/ServerPlaylistViewController.h
@@ -8,9 +8,29 @@
 
 #import <UIKit/UIKit.h>
 #import "PlaylistViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
+//#import "CAStreamBasicDescription.h"
+
+static const int kNumberBuffers = 3;
+// Create a data structure to manage information needed by the audio queue
+struct myAQStruct {
+    AudioFileID                     mAudioFile;
+    //CAStreamBasicDescription        mDataFormat;
+    AudioStreamBasicDescription     mDataFormat;
+    AudioQueueRef                   mQueue;
+    AudioQueueBufferRef             mBuffers[kNumberBuffers];
+    UInt32                          bufferByteSize;
+    SInt64                          mCurrentPacket;
+    UInt32                          mNumPacketsToRead;
+    AudioStreamPacketDescription    *mPacketDescs;
+    bool                            mIsRunning;
+    int  number;
+};
 
 @interface ServerPlaylistViewController : PlaylistViewController <MCNearbyServiceAdvertiserDelegate>{
     MCNearbyServiceAdvertiser *advert;
+    struct myAQStruct myinfo;
 }
 
 @end
--- SongTrain/ServerPlaylistViewController.m
+++ SongTrain/ServerPlaylistViewController.m
@@ -47,12 +47,26 @@
     advert.delegate = self;
     
     picker.delegate = self;
+    
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setActive:YES error:nil];
+    [audioSession setCategory:AVAudioSessionCategoryPlayback error:nil];
+    
+    [self getAudioFromFile: [[playlist firstObject] media]];
 }
 
 -(void)viewWillAppear:(BOOL)animated
 {
     NSLog(@"Advertising Peers...\n");
     [advert startAdvertisingPeer];
+
+    /*
+    const char *here = "somehting";
+     
+    dispatch_async(dispatch_queue_create(here, NULL),^{
+        [self getAudioFromFile: [[playlist firstObject] media]];
+    });
+     */
 }
 
 -(void)viewWillDisappear:(BOOL)animated
@@ -87,6 +101,210 @@
     
     [self dismissViewControllerAnimated:YES completion:nil];
     [mainTableView reloadData];
+}
+
+static char *FormatError(char *str, OSStatus error)
+{
+    // see if it appears to be a 4-char-code
+    *(UInt32 *)(str + 1) = CFSwapInt32HostToBig(error);
+    if (isprint(str[1]) && isprint(str[2]) && isprint(str[3]) && isprint(str[4])) {
+        str[0] = str[5] = '\'';
+        str[6] = '\0';
+    } else
+        // no, format it as an integer
+        sprintf(str, "%d", (int)error);
+    return str;
+}
+
+static void AQTestBufferCallback(void *inUserData, AudioQueueRef inAQ, AudioQueueBufferRef inCompleteAQBuffer) {
+    struct myAQStruct *myInfo = (struct myAQStruct *)inUserData;
+    NSLog(@"Buffer callback with misRunning: %d  with number : %d\n", myInfo->mIsRunning, myInfo->number);
+    if (myInfo->mIsRunning == 0) return;
+    NSLog(@"Filling Buffer\n");
+    UInt32 numBytes;
+    UInt32 nPackets = myInfo->mNumPacketsToRead;
+    
+    OSStatus i = AudioFileReadPackets (myInfo->mAudioFile, false, &numBytes, myInfo->mPacketDescs, myInfo->mCurrentPacket, &nPackets, inCompleteAQBuffer->mAudioData);
+    char error[7];
+    FormatError(error, i);
+    NSLog(@"AudioFileReadPackets with error code: %s\n", error);
+    
+    if (nPackets > 0) {
+        inCompleteAQBuffer->mAudioDataByteSize = numBytes;
+        i = AudioQueueEnqueueBuffer (inAQ, inCompleteAQBuffer, (myInfo->mPacketDescs ? nPackets : 0), myInfo->mPacketDescs);
+        NSLog(@"Just called enqueue buffer with error code: %d\n", i);
+        myInfo->mCurrentPacket += nPackets;
+    } else {
+        i = AudioQueueStop (myInfo->mQueue, false);
+        NSLog(@"Just called queue stop with error code: %d\n", i);
+        myInfo->mIsRunning = false;
+    }
+}
+
+void DeriveBufferSize (AudioStreamBasicDescription ASBDesc, UInt32 maxPacketSize, Float64 seconds, UInt32 *outBufferSize, UInt32 *outNumPacketsToRead) {
+    static const int maxBufferSize = 0x50000;
+    static const int minBufferSize = 0x4000;
+    
+    if (ASBDesc.mFramesPerPacket != 0) {
+        Float64 numPacketsForTime = ASBDesc.mSampleRate / ASBDesc.mFramesPerPacket * seconds;
+        *outBufferSize = numPacketsForTime * maxPacketSize;
+    } else {
+        *outBufferSize = maxBufferSize > maxPacketSize ? maxBufferSize : maxPacketSize;
+    }
+    
+    if (*outBufferSize > maxBufferSize && *outBufferSize > maxPacketSize)
+        *outBufferSize = maxBufferSize;
+    else {
+        if (*outBufferSize < minBufferSize)
+            *outBufferSize = minBufferSize;
+    }
+    
+    *outNumPacketsToRead = *outBufferSize / maxPacketSize;
+}
+
+- (void)getAudioFromFile:(MPMediaItem*)item{
+    
+    //NSURL *assetURL = [item valueForProperty:MPMediaItemPropertyAssetURL];
+    NSURL *assetURL = [NSURL URLWithString:[[NSBundle mainBundle] pathForResource:@"TheFuneral" ofType:@"m4a"]];
+    
+    //NSString *resourceURLString = [[[NSBundle mainBundle] resourceURL] absoluteString];
+    //NSURL *assetURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@TheFuneral.m4a", resourceURLString]];
+    
+    UInt32 dataSize;
+    OSStatus error;
+    char errorString[7];
+    
+    //struct myAQStruct myinfo;
+    myinfo.number = 76;
+    
+    NSLog(@"Attempt to open: %@", assetURL.absoluteString);
+    
+    if((error = AudioFileOpenURL((__bridge CFURLRef)assetURL, kAudioFileReadPermission, 0, &myinfo.mAudioFile))){
+        FormatError(errorString, error);
+        NSLog(@"Error opening url: %s\n", errorString);
+        return;
+    }
+    
+    
+    AudioFileGetPropertyInfo(myinfo.mAudioFile, kAudioFilePropertyDataFormat, &dataSize, NULL);
+    NSLog(@"Size of Data Format: %lu\n", (unsigned long)dataSize);
+    
+    if(AudioFileGetProperty(myinfo.mAudioFile, kAudioFilePropertyDataFormat, &dataSize, &myinfo.mDataFormat)){
+        NSLog(@"Error getting Property\n");
+        return;
+    }
+    
+    //CAStreamBasicDescription::Print();
+    
+    error = AudioQueueNewOutput (&myinfo.mDataFormat, AQTestBufferCallback, &myinfo, CFRunLoopGetCurrent(), kCFRunLoopCommonModes, 0, &myinfo.mQueue);
+    NSLog(@"Just called queue new output with error code: %d\n", error);
+    
+    
+    //Apple Example Code below
+    
+    UInt32 maxPacketSize;
+    UInt32 propertySize = sizeof (maxPacketSize);
+    AudioFileGetProperty (myinfo.mAudioFile, kAudioFilePropertyPacketSizeUpperBound, &propertySize, &maxPacketSize);
+    
+    DeriveBufferSize(myinfo.mDataFormat, maxPacketSize, 0.5, &myinfo.bufferByteSize, &myinfo.mNumPacketsToRead);
+    
+    NSLog(@"Buffer Byte size: %d\n", myinfo.bufferByteSize);
+    NSLog(@"Number of Packets to read size: %d\n", myinfo.mNumPacketsToRead);
+    
+    bool isFormatVBR = (myinfo.mDataFormat.mBytesPerPacket == 0 || myinfo.mDataFormat.mFramesPerPacket == 0);
+    
+    if (isFormatVBR) {
+        myinfo.mPacketDescs = (AudioStreamPacketDescription*) malloc (myinfo.mNumPacketsToRead * sizeof (AudioStreamPacketDescription));
+    } else {
+        myinfo.mPacketDescs = NULL;
+    }
+    
+    UInt32 cookieSize = sizeof (UInt32);
+    bool couldNotGetProperty = AudioFileGetPropertyInfo (myinfo.mAudioFile, kAudioFilePropertyMagicCookieData, &cookieSize,NULL);
+    
+    if (!couldNotGetProperty && cookieSize) {
+        char* magicCookie = (char *) malloc (cookieSize);
+        
+        AudioFileGetProperty (myinfo.mAudioFile, kAudioFilePropertyMagicCookieData, &cookieSize, magicCookie);
+        AudioQueueSetProperty (myinfo.mQueue, kAudioQueueProperty_MagicCookie, magicCookie, cookieSize);
+        
+        free (magicCookie);
+    }
+    myinfo.mIsRunning = true;
+    myinfo.mCurrentPacket = 0;
+    
+    
+    for (int i = 0; i < kNumberBuffers; ++i) {
+        error = AudioQueueAllocateBuffer (myinfo.mQueue, myinfo.bufferByteSize, &myinfo.mBuffers[i]);
+        NSLog(@"Just called queue allocate buffer with error code: %d\n", error);
+        AQTestBufferCallback(&myinfo, myinfo.mQueue, myinfo.mBuffers[i]);
+    }
+    
+    
+    
+    Float32 gain = 1.0;
+    // Optionally, allow user to override gain setting here
+    AudioQueueSetParameter (myinfo.mQueue, kAudioQueueParam_Volume, gain);
+    
+    myinfo.mIsRunning = true;
+    NSLog(@"Set is Running to: %d and number is: %d\n", myinfo.mIsRunning, myinfo.number);
+    
+    error = AudioQueueStart (myinfo.mQueue, NULL);
+    NSLog(@"Just called queue start with error code: %d\n", error);
+    
+    /*
+    do {
+        CFRunLoopRunInMode (kCFRunLoopDefaultMode, 0.25, false);
+        //NSLog(@"One time through loop\n");
+    } while (myinfo.mIsRunning);
+    
+    NSLog(@"After Loop\n");
+    CFRunLoopRunInMode (kCFRunLoopDefaultMode, 1, false);
+    */
+    
+     
+    //End Apple Example Code
+    
+    /*
+    i = AudioQueueSetParameter(myinfo.mQueue, kAudioQueueParam_Volume, 1);
+    NSLog(@"Just called queue set parameter with error code: %d\n", i);
+    
+    i = AudioQueueEnqueueBuffer(myinfo.mQueue, myinfo.mBuffers[0], 0, 0);
+    NSLog(@"Just called queue enqueue buffer with error code: %d\n", i);
+    
+    i = AudioQueuePrime(myinfo.mQueue, 0, NULL);
+    NSLog(@"Just called queue prime with error code: %d\n", i);
+    
+    i = AudioQueueStart(myinfo.mQueue, NULL);
+    NSLog(@"Just called queue start with error code: %d\n", i);
+    */
+    /*
+     CFDictionaryRef dic;
+     UInt32 dataSize;
+     
+     const void *keys;
+     const void *values;
+     
+    if(AudioFileGetProperty(inputFile, kAudioFilePropertyInfoDictionary, &dataSize, &dic)){
+        NSLog(@"Error getting Property\n");
+        return;
+    }
+
+    NSDictionary *newDict = (__bridge_transfer NSDictionary*)dic;
+    NSLog(@"Dictionary Count: %@\n", newDict);
+    
+    
+    
+    CFDictionaryGetKeysAndValues(dic, &keys, &values);
+    
+    NSLog(@"Key: %s\nValue: %s\n", ((char*)keys), ((char*)values));
+    
+    NSLog(@"Count: %lu\n", CFDictionaryGetCount(dic));
+    
+    for (int i = 0; i < CFDictionaryGetCount(dic); i++) {
+        NSLog(@"Key: %s\nValue: %s\n", ((char*)keys)[i], ((char*)values)[i]);
+    }
+     */
 }
 
 -(void)advertiser:(MCNearbyServiceAdvertiser *)advertiser didReceiveInvitationFromPeer:(MCPeerID *)peerID withContext:(NSData *)context invitationHandler:(void (^)(BOOL, MCSession *))invitationHandler
