2a8d5bb410aab060efc6fae47a3c49d1869b414e
Brandon Leventhal <bleventh@gmail.com>
2014-02-13 11:34:24 -0800

    Add some comments to animation for sanity

--- SongTrain/Animator.m
+++ SongTrain/Animator.m
@@ -59,9 +59,13 @@
     [container insertSubview:toViewController.view aboveSubview:newView];
 
 
-    // Animate the parallax
+    // Animate the parallax, push alternates for pushing and popping
+    // This might need to be changed for the info button, haven't attempted yet
     if (push) {
-           toViewController.view.frame = CGRectMake(fromViewController.view.frame.size.width, 0, fromViewController.view.frame.size.width, fromViewController.view.frame.size.height);
+
+        toViewController.view.frame = CGRectMake(fromViewController.view.frame.size.width, 0, fromViewController.view.frame.size.width, fromViewController.view.frame.size.height);
+
+        // Timing can be altered
         [UIView animateKeyframesWithDuration:2 delay:0 options:0 animations:^{
             newView.transform = CGAffineTransformMakeTranslation(-fromViewController.view.frame.size.width / 2, 0);
             toViewController.view.transform = CGAffineTransformMakeTranslation(-fromViewController.view.frame.size.width, 0);
