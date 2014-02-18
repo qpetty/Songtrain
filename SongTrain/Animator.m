d29c071236646edd6b68a06d7e5477839a5f207d
Brandon Leventhal <bleventh@gmail.com>
2014-02-17 20:52:48 -0800

    Add some Library additions just in case we want to turn this into a library

--- SongTrain/Animator.m
+++ SongTrain/Animator.m
@@ -23,6 +23,16 @@
     return self;
 }
 
+- (instancetype)initWithImage:(UIImageView *)parallaxImage
+{
+    self = [super init];
+    if (self) {
+        self.firstTime = YES;
+    }
+    // Consider making all this generic to be turned into a library
+    return self;
+}
+
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
 {
     // Get to and from view controllers from the context using given keys
@@ -67,9 +77,14 @@
 
         toViewController.view.frame = CGRectMake(fromViewController.view.frame.size.width, 0, fromViewController.view.frame.size.width, fromViewController.view.frame.size.height);
 
-        // Timing can be altered
         [UIView animateKeyframesWithDuration:TRANSITION_TIME delay:0 options:0 animations:^{
             newView.frame = CGRectMake(newView.frame.origin.x - (newView.frame.size.width - toViewController.view.frame.size.width)/NUMBER_OF_VIEWS, newView.frame.origin.y, newView.frame.size.width, newView.frame.size.height);
+        } completion:^(BOOL finished) {
+            //[transitionContext completeTransition:finished];
+        }];
+        // Timing can be altered
+        [UIView animateKeyframesWithDuration:TRANSITION_TIME delay:0 options:0 animations:^{
+            //newView.frame = CGRectMake(newView.frame.origin.x - (newView.frame.size.width - toViewController.view.frame.size.width)/NUMBER_OF_VIEWS, newView.frame.origin.y, newView.frame.size.width, newView.frame.size.height);
             toViewController.view.frame = CGRectMake(toViewController.view.frame.origin.x - toViewController.view.frame.size.width, toViewController.view.frame.origin.y, toViewController.view.frame.size.width, toViewController.view.frame.size.height);
 
             fromViewController.view.frame = CGRectMake(fromViewController.view.frame.origin.x - fromViewController.view.frame.size.width, fromViewController.view.frame.origin.y, fromViewController.view.frame.size.width, fromViewController.view.frame.size.height);
