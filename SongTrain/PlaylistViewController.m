601cd57367c7dd0fee74632b87f87dc27ed04fac
Brandon Leventhal <bleventh@gmail.com>
2014-04-12 22:44:03 -0700

    Thread tableview reload

--- SongTrain/PlaylistViewController.m
+++ SongTrain/PlaylistViewController.m
@@ -215,7 +215,9 @@
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
-    [mainTableView reloadData];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [mainTableView reloadData];
+    });
 }
 
 @end
