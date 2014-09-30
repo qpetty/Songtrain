63d423998ca375d7d06e959e62d86dc94d0fd294
Quinton Petty <qlpetty@gmail.com>
2014-09-29 17:52:43 -0700

    Got rid of the additional uicolor macros

--- Songtrain/ViewController.m
+++ Songtrain/ViewController.m
@@ -13,13 +13,6 @@
 #import "PeerTableViewCell.h"
 #import "QPSessionManager.h"
 
-#ifndef HEX_COLOR
-#define HEX_COLOR
-#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
-
-#define UIColorFromRGBWithAlpha(rgbValue, alp) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha: alp]
-#endif
-
 @interface ViewController ()
 
 @end
