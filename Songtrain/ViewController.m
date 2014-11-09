1170687fc188e66f4f7e570846aafa2911bba49a
Quinton Petty <qlpetty@gmail.com>
2014-11-08 23:22:07 -0800

    Updates album artowrk even when already curled

--- Songtrain/ViewController.m
+++ Songtrain/ViewController.m
@@ -249,7 +249,22 @@
     }
     self.currentAlbumArtwork.image = image;
     
+    if (curlView) {
+        [curlView drawImageOnFrontOfPage:[self imageWithImage:image scaledToSize:curlView.frame.size]];
+    }
+    
     [musicPlayer updateNowPlaying];
+}
+
+- (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
+    //UIGraphicsBeginImageContext(newSize);
+    // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
+    // Pass 1.0 to force exact pixel size.
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 -(IBAction)editAllTableViews:(id)sender {
